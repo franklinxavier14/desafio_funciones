14a7a129a3ca9754ca693f59e359a8afae9993bc
franklinxavier <franxmb@gmail.com>
2023-11-26 10:46:15 -0300

    se modifican los js

--- assets/js/1_funcion.js
+++ assets/js/1_funcion.js
@@ -1,3 +1,13 @@
+// Ejemplo Guia Desafio
+
+// function example(a, b, c){
+//     return a+b+c
+//     }
+    
+
+
 //Pasando a una funcion de expresion
 
-const example = (a, b, c) => a + b + c;
+example = function (a, b, c){
+    return a + b + c
+}
--- assets/js/2_arrow.js
+++ assets/js/2_arrow.js
@@ -1,3 +1,12 @@
+
+// Ejemplo del Deasafio
+
+// let suma = function(a, b){
+//     return a + b
+// }
+
+
+
 //Transformadno funcion a una arrow function de una linea
 
 const suma = (a, b) => a + b;
